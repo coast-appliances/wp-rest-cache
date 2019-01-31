4bac38e73d1f1d07737edd36f1b2652db9363321
acato <acato@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-01-31 09:07:47 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/wp-rest-cache/trunk@2022528 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/partials/caches-table.php
@@ -0,0 +1,19 @@
+<?php
+$wp_rest_cache_sub = filter_input( INPUT_GET, 'sub', FILTER_SANITIZE_STRING );
+?>
+<div id="poststuff">
+    <div id="post-body" class="metabox-holder">
+        <div class="meta-box-sortables ui-sortable">
+            <form method="post">
+                <input type="hidden" name="page" value="wp-rest-cache"/>
+                <input type="hidden" name="sub" value="<?php echo esc_attr( $wp_rest_cache_sub ); ?>"/>
+                <?php
+                $wp_rest_cache_list->prepare_items();
+                $wp_rest_cache_list->search_box( __( 'Search', 'wp-rest-cache' ), 'search_id' );
+                $wp_rest_cache_list->display();
+                ?>
+            </form>
+        </div>
+    </div>
+</div>
+<br class="clear">
---
+++ admin/partials/sub-endpoint-api.php
@@ -0,0 +1,6 @@
+<div class="wrap">
+    <?php
+    $wp_rest_cache_list = new \WP_Rest_Cache_Plugin\Admin\Includes\API_Caches_Table( 'endpoint' );
+    include_once( 'caches-table.php' );
+    ?>
+</div>
